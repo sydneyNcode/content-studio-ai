db09c7d32e3d3a45c094f783af047e15614af429
Sydney <sydneyhoeft@gmail.com>
2026-06-03 20:08:03 -0700

    Fix Layout layout

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -3,18 +3,9 @@ import BottomNav from './BottomNav'
 
 export default function Layout({ children }) {
   return (
-    <div
-      className="bg-[#FAF8F5] dark:bg-[#18120F] text-[#1A1008] dark:text-[#F0EBE5] font-['Poppins']"
-      style={{ height: '100%', display: 'flex', flexDirection: 'column' }}
-    >
+    <div className="min-h-screen bg-[#FAF8F5] dark:bg-[#18120F] text-[#1A1008] dark:text-[#F0EBE5] font-['Poppins']">
       <Header />
-      <main style={{
-        flex: 1,
-        overflowY: 'auto',
-        WebkitOverflowScrolling: 'touch',
-        overscrollBehavior: 'contain',
-        paddingBottom: '80px'
-      }}>
+      <main className="pb-24">
         {children}
       </main>
       <BottomNav />
